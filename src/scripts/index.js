58c0bba48f72ffc64042fd43fe69fc07e0359645
GabrielBRQ <gabrielmedvalle@gmail.com>
2024-03-04 20:06:51 -0300

    add projects transition

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,30 @@
-import _ from 'lodash';
+import _, { entries } from 'lodash';
 import '../css/style.css';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('nav_check');
     var closeButton = document.querySelector('.close-button');
     var leftElement = document.querySelector('.left');
+    
 
-    checkbox.addEventListener('change', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => {observer.observe(el)});
+
+    checkbox.addEventListener('change', function () {
         leftElement.style.left = '0';
     });
 
-    closeButton.addEventListener('click', function (){
+    closeButton.addEventListener('click', function () {
         leftElement.style.left = '';
     });
 });
