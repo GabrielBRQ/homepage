243e8ba77dc4377d361239f3344f048be6ba648e
GabrielBRQ <gabrielmedvalle@gmail.com>
2024-03-04 17:38:15 -0300

    add Responsive Design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,16 @@
 import _ from 'lodash';
 import '../css/style.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.getElementById('nav_check');
+    var closeButton = document.querySelector('.close-button');
+    var leftElement = document.querySelector('.left');
+
+    checkbox.addEventListener('change', function() {
+        leftElement.style.left = '0';
+    });
+
+    closeButton.addEventListener('click', function (){
+        leftElement.style.left = '';
+    });
+});
